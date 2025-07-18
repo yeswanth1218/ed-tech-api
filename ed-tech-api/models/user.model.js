f4370169e78d77bb6a70e3e1ad0d71cad875ab05
Ravi Kumar Patel <Ravi.Patel@almonds.ai>
2025-07-18 23:29:19 +0530

    minor vh

--- ed-tech-api/models/user.model.js
+++ ed-tech-api/models/user.model.js
@@ -1,12 +1,18 @@
+// models/user.model.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const User = sequelize.define('users', {
-  name: { type: DataTypes.STRING },
+  name: { type: DataTypes.STRING, allowNull: true },
+  class: { type: DataTypes.INTEGER, allowNull: true },
+  section: { type: DataTypes.STRING, allowNull: true },
   mobile: { type: DataTypes.STRING },
   username: { type: DataTypes.STRING, unique: true },
   password: { type: DataTypes.STRING },
-  role: { type: DataTypes.STRING }
+  role: { 
+    type: DataTypes.ENUM('STUDENT', 'TEACHER', 'ADMIN'),
+    allowNull: false
+  }
 });
 
 module.exports = User;
--- ed-tech-api/validations/auth.validation.js
+++ ed-tech-api/validations/auth.validation.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const registerSchema = Joi.object({
   username: Joi.string().min(3).max(100).required(),
   password: Joi.string().min(6).required(),
-  role: Joi.string().valid('admin', 'student', 'teacher').required(),
+  role: Joi.string().valid('STUDENT', 'TEACHER', 'ADMIN').required(),
 });
 
 const loginSchema = Joi.object({
